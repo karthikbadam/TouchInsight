7f2f19b633f30475c636ebe6c872739e9ff2f3f6
karthikbadam <sbadam@purdue.edu>
2015-08-26 18:15:24 -0400

    flights

---
+++ js/touchinsight-client.js
@@ -0,0 +1,17 @@
+
+var sourceID = "SID";
+var destID = "DID";
+var source = "Source";
+var destination = "Destination";
+var passengers = "Passengers";
+var seats = "Seats";
+var numFlights = "Flights";
+var distance = "Distance";
+var date = "Date";
+var sourcePopulation = "SPopulation";
+var destPopulation = "DPopulation";
+
+var dataFile = "/data/flights.csv";
+
+
+
